6a8e3e9d4cda54319ed46676d6e02e35704d22bb
bujosa <davidbujosa@gmail.com>
2021-06-07 11:18:12 -0400

    Added new packages

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,45 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  UploadedFile,
+  UploadedFiles,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post('upload')
+  @UseInterceptors(
+    FileInterceptor('photo', {
+      limits: {
+        fileSize: 2000000,
+      },
+    }),
+  )
+  async uploadFile(@UploadedFile() file: Express.Multer.File) {
+    return await this.appService.uploadFile(file);
+  }
+
+  @Get('delete/:id')
+  async deleteFile(@Param() params) {
+    return await this.appService.deleteFile(params.id);
+  }
+
+  @Post('uploads')
+  @UseInterceptors(
+    FilesInterceptor('photos', null, {
+      limits: { fileSize: 2000000 },
+    }),
+  )
+  async uploadFiles(
+    @UploadedFiles() files: Express.Multer.File[],
+  ): Promise<string[]> {
+    return await this.appService.uploadFiles(files);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { uploadFileHelper } from './helper/upload-file';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor() {
+    admin.initializeApp({
+      credential: admin.credential.cert('./settings.json'),
+    });
+  }
+
+  async uploadFile(file: Express.Multer.File): Promise<string> {
+    return await uploadFileHelper(admin.storage(), file);
+  }
+
+  async deleteFile(key: string) {
+    const bucket = admin.storage().bucket(process.env.BUCKET);
+    try {
+      bucket.file(key).delete();
+    } catch (err) {
+      console.log(err);
+      return false;
+    }
+    return true;
+  }
+
+  async uploadFiles(files: Express.Multer.File[]): Promise<string[]> {
+    const urls = [];
+    files.map(async (file: Express.Multer.File) => {
+      urls.push(await uploadFileHelper(admin.storage(), file));
+    });
+    return await urls;
   }
 }
