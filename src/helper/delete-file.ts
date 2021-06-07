d2ac3f37a08239e3782934c73d54ab9c6fbceaa1
bujosa <davidbujosa@gmail.com>
2021-06-07 12:52:30 -0400

    Finished delete file

--- src/helper/delete-file.ts
+++ src/helper/delete-file.ts
@@ -2,8 +2,7 @@ import { Endpoint, S3 } from 'aws-sdk';
 import { Picture } from 'src/dto/picture';
 
 export const deleteFileHelper = async (picture: Picture) => {
-  const key = picture.url.split(`${process.env.SPACE_ENDPOINT}\\`).pop();
-  console.log(key);
+  const key = picture.url.split(`${process.env.SPACE_ENDPOINT}/`).pop();
   const spacesEndpoint = new Endpoint(process.env.SPACE_ENDPOINT);
   const s3 = new S3({ endpoint: spacesEndpoint });
   return await s3
