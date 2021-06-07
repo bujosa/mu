84b98159529656fb34d048d0ee95e89ebbfd0763
bujosa <davidbujosa@gmail.com>
2021-06-07 13:15:04 -0400

    Fixed key

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { config } from 'aws-sdk';
 async function bootstrap() {
   config.update({
     accessKeyId: process.env.AWS_ACCESS_KEY,
-    secretAccessKey: process.env.AWS_SECRET_KET,
+    secretAccessKey: process.env.AWS_SECRET_KEY,
     region: process.env.AWS_REGION,
   });
   const app = await NestFactory.create(AppModule);
