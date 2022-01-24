b11d6d5852966a4a3ff146dd261c4507235e85a9
bujosa <davidbujosa@gmail.com>
2022-01-24 20:55:17 +0100

    Separate variables

--- src/helper/delete-file.ts
+++ src/helper/delete-file.ts
@@ -5,6 +5,7 @@ export const deleteFileHelper = async (picture: Picture) => {
   const key = picture.url.split(`${process.env.SPACE_ENDPOINT}/`).pop();
   const spacesEndpoint = new Endpoint(process.env.SPACE_ENDPOINT);
   const s3 = new S3({ endpoint: spacesEndpoint });
+
   return await s3
     .deleteObject({
       Bucket: process.env.BUCKET_NAME,
